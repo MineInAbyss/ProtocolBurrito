135be5d70e81bda009cf4c475aa3a1a1e1f1d36c
0ffz <dan.voznyy@gmail.com>
2021-06-08 12:55:03 -0400

    Switch from pdm to slimjar, update to Kotlin 1.5.10

--- src/main/kotlin/com/mineinabyss/protocolburrito/ProtocolBurritoPlugin.kt
+++ src/main/kotlin/com/mineinabyss/protocolburrito/ProtocolBurritoPlugin.kt
@@ -1,14 +1,14 @@
 package com.mineinabyss.protocolburrito
 
 import com.mineinabyss.protocolburrito.generation.RunGenerator
-import me.bristermitten.pdm.PluginDependencyManager
+import io.github.slimjar.app.builder.ApplicationBuilder
 import org.bukkit.plugin.java.JavaPlugin
 
 @RunGenerator
 class ProtocolBurritoPlugin : JavaPlugin() {
     override fun onEnable() {
         logger.info("Downloading dependencies.")
-        PluginDependencyManager.of(this).loadAllDependencies().join()
+        ApplicationBuilder.appending("ProtocolBurrito").build()
         logger.info("ProtocolBurrito is ready to start wrapping packets!")
     }
 }
