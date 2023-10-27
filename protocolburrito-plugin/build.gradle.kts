7b2b0218f29f9c1b995d2a092b17756f3296a5d5
Boy <sivertpaulsen2@gmail.com>
2023-10-27 15:01:11 +0200

    convert to paper-plugin

--- protocolburrito-plugin/build.gradle.kts
+++ protocolburrito-plugin/build.gradle.kts
@@ -9,6 +9,6 @@ plugins {
 }
 
 dependencies {
+    compileOnly(libs.bundles.idofront.core)
     implementation(project(":"))
-    implementation(libs.bundles.idofront.core)
 }
--- protocolburrito-plugin/src/main/kotlin/com/mineinabyss/protocolburrito/ProtocolBurritoPlugin.kt
+++ protocolburrito-plugin/src/main/kotlin/com/mineinabyss/protocolburrito/ProtocolBurritoPlugin.kt
@@ -1,12 +1,8 @@
 package com.mineinabyss.protocolburrito
 
-import com.mineinabyss.idofront.platforms.Platforms
 import org.bukkit.plugin.java.JavaPlugin
 
 class ProtocolBurritoPlugin : JavaPlugin() {
-    override fun onLoad() {
-        Platforms.load(this, "mineinabyss")
-    }
     override fun onEnable() {
         logger.info("ProtocolBurrito is ready to start wrapping packets!")
     }
